23a98972700641f711252367439f89bef9a6bb98
Brynna.we <Brynna.we@gmail.com>
2021-08-25 18:35:18 +0900

    0825커밋

---
+++ evergreen-ui-work/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SearchInputBox from './SearchInputBox.js';
+
+function App() {
+  return (
+<div>
+<SearchInputBox/>
+</div>
+  );
+}
+
+export default App;
---
+++ evergreen-ui-work/src/SearchInputBox.js
@@ -0,0 +1,43 @@
+import React, {
+  Component
+} from 'react';
+import {
+  SearchInput
+} from 'evergreen-ui';
+
+class SearchInputBox extends Component {
+
+  state = {
+    inputValue: ''
+  }
+
+  setover=(text)=>{
+    let inputValue='';
+    inputValue = text;
+    this.setState({inputValue});
+  }
+
+  check=()=> console.log(this.state.inputValue);
+
+  render() {
+
+    const {
+      inputValue
+    } = this.state;
+
+    return ( 
+      <div>
+      <SearchInput 
+          placeholder = "Filter traits..."
+          value = {inputValue}
+          onChange = {(e) => this.setover(e.target.value)}
+          onClick = {()=>this.setover('')}
+      />
+      <button type="button" onClick={this.check}>확인</button>
+      </div>);
+  };
+
+}
+
+
+export default SearchInputBox;
